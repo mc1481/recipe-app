433561cbc72383119e4585b1fc2a08630c2e459a
mc1481 <mariacrowley01@gmail.com>
2023-11-27 18:08:06 +0000

    change to hashrouter and set up gh-pages

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
